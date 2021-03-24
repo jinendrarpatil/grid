30f002486270fe6664a4f7b2b1ccdf86f637fb91
jinendrarpatil <jinendrarpatil@gmail.com>
2021-03-24 17:06:30 -0400

    1st

---
+++ projectList.js
@@ -0,0 +1,20 @@
+let projectList=[
+    {
+        heading:"Semarang EOR Brownfield
+        Engg",
+        contractor:"Petra Resources Sdn. Bhd.",
+        client:"Petronas upstream(Carigali)",
+        src:"https://www.researchgate.net/profile/Pragyaditya-Das/publication/287806924/figure/fig1/AS:668966468980744@1536505443526/Block-diagram-of-a-steam-power-plant.png",
+        l1:"PDMS Models for SUPG-B & SMDP-B Process
+        platforms.",
+        l2:"Stress analysis of Piping system to include drag
+        load due blast case scenario for Process, fire
+        water GRE Piping Systems.",
+        l3:"Isometrics & Work pack updation.",
+        l4:"Consultation on Constructability, Offshore
+        Installation using Mechanical connectors.",
+        l5:""
+    }
+]
+
+export default projectList;
